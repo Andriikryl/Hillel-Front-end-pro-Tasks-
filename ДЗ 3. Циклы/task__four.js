9292fe17ff5137db72c3cc5c4789515eaecc6684
Amdrii <andreyk557@gmail.com>
2022-08-12 18:27:02 +0300

    task four /

--- ДЗ 3. Циклы/task__four.js
+++ ДЗ 3. Циклы/task__four.js
@@ -1,3 +1,14 @@
 // Дано целое число (ввести через prompt). Выяснить, является ли оно простым (простым называется число, больше 1, не имеющих других делителей кроме 1 и себя).
 // Пример простых чисел: 3, 17, 7, 5
 
+const num = +prompt();
+let isSimple = true;
+
+for (let i = num - 1; i > 1; i-- ){
+    if (num % i === 0){
+        isSimple = false;
+        break;
+    }
+}
+
+console.log(isSimple ? "simple" : "not simple");
