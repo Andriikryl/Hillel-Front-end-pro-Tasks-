03d86ddf85f2a8a2a1ce658d991f8dcbadcc36af
Amdrii <andreyk557@gmail.com>
2022-08-18 13:16:25 +0300

    tasksre

---
+++ ДЗ 5. Объекты и функции/Task__two.js
@@ -0,0 +1,11 @@
+// 2 Реализовать функцию removeElement(array, item) чтобы удалить элемент item из массива array.
+
+
+
+
+function removeElement(array, item){
+    const index = array.indexOf(item);
+    const removed = array.splice(index, 1);
+    console.log(removed);
+}
+
