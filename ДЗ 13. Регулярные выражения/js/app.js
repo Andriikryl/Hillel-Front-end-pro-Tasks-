dfaff85f944d656c0ad0b2e9a0ebb7f0ecce8b19
Amdrii <andreyk557@gmail.com>
2022-10-28 16:42:06 +0300

    anim

--- ДЗ 13. Регулярные выражения/js/app.js
+++ ДЗ 13. Регулярные выражения/js/app.js
@@ -7,68 +7,6 @@
 // Прочитать документацию о регулярных выражениях
 
 
-
-let basicTimeline = anime.timeline({
-  autoplay: false
-});
-
-var pathEls = $(".check");
-for (var i = 0; i < pathEls.length; i++) {
-  var pathEl = pathEls[i];
-  var offset = anime.setDashoffset(pathEl);
-  pathEl.setAttribute("stroke-dashoffset", offset);
-}
-
-basicTimeline
-  .add({
-    targets: ".text",
-    duration: 1,
-    opacity: "0"
-  })
-  .add({
-    targets: ".button",
-    duration: 1300,
-    height: 10,
-    width: 300,
-    backgroundColor: "#2B2D2F",
-    border: "0",
-    borderRadius: 100
-  })
-  .add({
-    targets: ".progress-bar",
-    duration: 2000,
-    width: 300,
-    easing: "linear"
-  })
-  .add({
-    targets: ".button",
-    width: 0,
-    duration: 1
-  })
-  .add({
-    targets: ".progress-bar",
-    width: 300,
-    height: 472,
-    delay: 500,
-    duration: 750,
-    borderRadius: 80,
-    backgroundColor: "#f4e3d1"
-  })
-  .add({
-    targets: pathEl,
-    strokeDashoffset: [offset, 0],
-    duration: 200,
-    easing: "easeInOutSine"
-  });
-
-$(".button").click(function() {
-  basicTimeline.play();
-});
-
-$(".text").click(function() {
-  basicTimeline.play();
-});
-
 const SUCCESS_CLASSNAME = "success";
 const ERROR_CLASSNAME = "error";
 
