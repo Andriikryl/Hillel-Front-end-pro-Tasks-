0d5ac441aace3b9f39cd7424b0eee66979b7df65
Amdrii <andreyk557@gmail.com>
2022-10-28 16:07:49 +0300

    init

--- ДЗ 18. Виджет с погодой/js/app.js
+++ ДЗ 18. Виджет с погодой/js/app.js
@@ -19,19 +19,19 @@
 
 "use strict";
 
-const url =
-  "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19";
+const url ="http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19";
 const wetherList = document.getElementById("wether-list");
 
 fetch(url)
   .then((response) => response.json())
   .then(({ main, wind, weather }) => {
+    
     wetherList.innerHTML = `
     <h1> ${main.temp}°C</h1>
     <p>Wind: ${wind.speed} km/h ${wind.deg}°</p> 
        <p>Pressure: ${main.pressure} hPa</p>
     <p>Humidity: ${main.humidity} %</p> `;
-
+    
     weather.forEach(function (user) {
       const block = document.createElement("div");
       block.innerHTML = `
@@ -39,4 +39,5 @@ fetch(url)
         <p> ${user.description}</p> `;
       wetherList.append(block);
     });
+
   });
